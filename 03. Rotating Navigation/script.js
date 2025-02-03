bea42ac732cab5d2e0ccb220c4d2e77ef9034ad2
ephraim <ephraims.818@gmail.com>
2025-02-03 14:26:32 +0600

    JS--PROJECTS--50D-50P - 03. Rotating Navigation

---
+++ 03. Rotating Navigation/script.js
@@ -0,0 +1,10 @@
+"use strict";
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
+
+open.addEventListener("click", function () {
+    container.classList.add("show-nav");
+});
+
+close.addEventListener("click", () => container.classList.remove("show-nav"));
